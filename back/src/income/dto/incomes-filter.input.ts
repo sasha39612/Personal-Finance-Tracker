e1cc511da62d7943760f92a8c04cc68b322e5b52
Oleksandr Stolyarov <stolyarov1oleksandr@gmail.com>
2025-09-02 03:50:03 +0200

    Cleaned income

--- back/src/income/dto/incomes-filter.input.ts
+++
@@ -1,7 +0,0 @@
-import { Field, InputType } from '@nestjs/graphql';
-
-@InputType()
-export class IncomeFilterInput {
-  @Field({ nullable: true })
-  datum?: Date;
-}
--- back/src/income/income.module.ts
+++ back/src/income/income.module.ts
@@ -5,20 +5,12 @@ import { Category } from './entities/categories.entity';
 import { Entities } from './entities/entities.entity';
 import { IncomeResolver } from './income.resolver';
 import { IncomeService } from './income.service';
-import { CategoryIncomeResolver } from './category-income.resolver';
-import { CategoryIncomeService } from './category-income.service';
-import { EntitiesIncomeResolver } from './entities-income.resolver';
-import { EntitiesIncomeService } from './entities-income.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Income, Category, Entities])],
   providers: [
     IncomeResolver,
     IncomeService,
-    CategoryIncomeResolver,
-    CategoryIncomeService,
-    EntitiesIncomeResolver,
-    EntitiesIncomeService,
   ],
   exports: [TypeOrmModule, IncomeService],
 })
